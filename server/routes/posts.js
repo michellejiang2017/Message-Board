a0384fda07fa923235780a6e5db25453405526c2
Michelle Jiang <mhjiang@smith.edu>
2026-05-25 09:47:46 -0400

    update

--- server/routes/posts.js
+++ server/routes/posts.js
@@ -1,11 +1,18 @@
 const express = require("express");
 const router = express.Router();
 
-// Get all users
 router.get("/", (req, res) => {
     let ret = [];
     // Do things to get the data...
     res.status(200).json(ret);
 });
+
+router.get("/:id", (req, res) => {
+    const id = req.params.id;
+    // Do the same as before
+    res.status(200).json({ id });
+});
+
+// other routes
 
 module.exports = router;
